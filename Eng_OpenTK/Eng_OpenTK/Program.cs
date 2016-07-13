2b01981f82828741e8569c91a6a33238c99d1cf7
piotrek.hejmej <piotrek10h@gmail.com>
2016-07-13 22:21:47 +0200

    Setting Up glut rener window 1280x720

--- Eng_OpenTK/Eng_OpenTK/Program.cs
+++ Eng_OpenTK/Eng_OpenTK/Program.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenGL;
+using Tao.FreeGlut;
+
+
 
 namespace Eng_OpenTK
 {
     class Program
     {
+        private static int width = 1280, height = 720;
+
         static void Main(string[] args)
         {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL three dimensional grid");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
             
+            Glut.glutMainLoop();
+        }
+
+        private static void OnDisplay() { }
+
+        private static void OnRenderFrame()
+        {
+            
+
+
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Glut.glutSwapBuffers(); 
         }
     }
 }

d42ced3cc81a83c6d42247ac8867d9ea8a50e9ff
phejmej <phejmej@PLKRBSUPSXM3L>
2016-07-14 13:11:37 +0200

    Building 2d squares grid

--- Eng_OpenTK/Eng_OpenTK/Program.cs
+++ Eng_OpenTK/Eng_OpenTK/Program.cs
@@ -10,12 +10,37 @@ using Tao.FreeGlut;
 
 namespace Eng_OpenTK
 {
+
+    
+
     class Program
     {
+        public class forSquare
+        {
+            public static VBO<Vector3>[] square;
+            public static int[] x = new int[10];
+
+            public forSquare (int count)
+            {
+                square =  new VBO<Vector3>[count];
+            }
+
+            public void calculateMatrix(int length,int count)
+            {
+                
+                for (int i=0; i < 10; i++)
+                    for (int j = 0; j < 10; j++)
+                    {
+                        square[i * 10 + j] = new VBO<Vector3>(new Vector3[] { new Vector3(i, j, 0), new Vector3(i + length, j, 0), new Vector3(i + length, j + length, 0), new Vector3(i, j + length, 0) });
+                    }
+            }
+        }
+
         private static int width = 1280, height = 720;
         private static ShaderProgram program;
         private static VBO<Vector3> triangle, square;
         private static VBO<int> triangleElements, squareElements;
+        
 
         static void Main(string[] args)
         {
@@ -27,11 +52,26 @@ namespace Eng_OpenTK
             Glut.glutIdleFunc(OnRenderFrame);
             Glut.glutDisplayFunc(OnDisplay);
 
+           
+
             program = new ShaderProgram(VertexShader, FragmentShader);
 
             program.Use();
             program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
-            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 10), Vector3.Zero, Vector3.Up));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 100), Vector3.Zero, Vector3.Up));
+
+            forSquare squares = new forSquare(100);
+
+            Console.WriteLine(forSquare.x[0]);
+            try
+            {
+                squares.calculateMatrix(1, 100);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Something went wrong");
+            }
+
 
             triangle = new VBO<Vector3>(new Vector3[] { new Vector3(0, 1, 0), new Vector3(-1, -1, 0), new Vector3(1, -1, 0) });
             square = new VBO<Vector3>(new Vector3[] { new Vector3(-1, 1, 0), new Vector3(1, 1, 0), new Vector3(1, -1, 0), new Vector3(-1, -1, 0) });
@@ -78,12 +118,20 @@ namespace Eng_OpenTK
 
             //square
             program["model_matrix"].SetValue(Matrix4.CreateTranslation(new Vector3(1.5f, 0, 0)));
-
-            Gl.BindBufferToShaderAttribute(square, program, "vertexPosition");
-            Gl.BindBuffer(squareElements);
-
-            Gl.DrawElements(BeginMode.Quads, squareElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
-            
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Gl.BindBufferToShaderAttribute(forSquare.square[i], program, "vertexPosition");
+                    Gl.BindBuffer(squareElements);
+                    
+                    Gl.DrawElements(BeginMode.Quads, squareElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Something went wrong");
+            }
 
 
             Glut.glutSwapBuffers(); 
